b806c662993424978317816f4dd4187e6b501749
Pankaj Patidar <pankaj9296@gmail.com>
2022-05-31 13:43:53 +0530

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: 'WIKI',
+    script: './build/server/index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 2,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '500M',
+    instance_var: 'INSTANCE_ID',
+  }],
+};
