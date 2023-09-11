80044f8f2de78bbf83995c73057918404dc8efa9
Pankaj Patidar <pankaj9296@gmail.com>
2023-09-11 14:55:45 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: './build/server/index.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    instances: 2,
+    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '500M',
